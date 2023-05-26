ed3fa563c4b0135901011419787980a3b80b4600
Ivo Zivkov <izivkov@gmail.com>
2023-05-26 11:16:01 -0400

    updated docs.

--- api/src/main/java/org/avmedia/gshockapi/WatchInfo.kt
+++ api/src/main/java/org/avmedia/gshockapi/WatchInfo.kt
@@ -1,5 +1,9 @@
 package org.avmedia.gshockapi
 
+/**
+ * This class keeps track of what is the model and name of the Watch.
+ * Currently supported models are: B2100, B5600 (which also includes the B5000, but they have identical functionality)
+ */
 object WatchInfo {
 
     enum class WATCH_MODEL {
@@ -9,6 +13,9 @@ object WatchInfo {
     var model = WATCH_MODEL.UNKNOWN
     private var deviceName: String = ""
 
+    /**
+     * Sets the name of the watch, usually obtained from BLE scanning.
+     */
     fun setDeviceName(name: String) {
         deviceName = name
 
@@ -19,6 +26,9 @@ object WatchInfo {
         }
     }
 
+    /**
+     * Returns name of the watch.
+     */
     fun getDeviceName(): String {
         return deviceName
     }
