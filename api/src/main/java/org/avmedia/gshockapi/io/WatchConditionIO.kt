d55ec2aefb51b2aacfa2033ca6ec38f0a6539a9a
Ivo Zivkov <izivkov@gmail.com>
2023-09-17 18:16:00 -0400

    Fine-adjust battery level

--- api/src/main/java/org/avmedia/gshockapi/io/WatchConditionIO.kt
+++ api/src/main/java/org/avmedia/gshockapi/io/WatchConditionIO.kt
@@ -4,6 +4,8 @@ import kotlinx.coroutines.CompletableDeferred
 import org.avmedia.gshockapi.WatchInfo
 import org.avmedia.gshockapi.utils.Utils
 import org.json.JSONObject
+import timber.log.Timber
+import kotlin.math.roundToInt
 
 object WatchConditionIO {
 
@@ -50,11 +52,14 @@ object WatchConditionIO {
             val bytes = Utils.byteArrayOfIntArray(intArr.drop(1).toIntArray())
 
             if (bytes.size >= 2) {
-                // Battery level between 15 and 20 fot B2100 and between 13 and 18 for B5600. Scale accordingly to %
-                val batteryLevel =
-                    bytes[0].toInt() - if (WatchInfo.model == WatchInfo.WATCH_MODEL.GA) 15 else 13
+                // Battery level between 15 and 20 fot B2100 and between 13 and 19 for B5600. Scale accordingly to %
+                Timber.i("battery level row value: ${bytes[0].toInt()}")
 
-                val batteryLevelPercent: Int = (batteryLevel * 20).coerceIn(0, 100)
+                val batteryLevelLowerLimit = if (WatchInfo.model == WatchInfo.WATCH_MODEL.GA) 15 else 13
+                val batteryLevelUpperLimit = if (WatchInfo.model == WatchInfo.WATCH_MODEL.GA) 20 else 19
+                val multiplier:Int = (100.0 / (batteryLevelUpperLimit - batteryLevelLowerLimit)).roundToInt()
+                val batteryLevel = (bytes[0].toInt() - batteryLevelLowerLimit)
+                val batteryLevelPercent: Int = (batteryLevel * multiplier).coerceIn(0, 100)
                 val temperature: Int = bytes[1].toInt()
 
                 return WatchConditionValue(batteryLevelPercent, temperature)
